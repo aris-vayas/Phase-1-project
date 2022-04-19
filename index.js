2c3cc0d15aba4bcbc1ba1ae40d411fc6d8dccfff
Hyun Sung Cho <hcho774@gmail.com>
2022-04-19 15:45:03 -0400

    js SearchBox

--- index.js
+++ index.js
@@ -7,56 +7,78 @@
         //ener like
         //listener event for like 
 
-        let divCard = document.getElementById("anime-collection");
-        let searchBtn = document.getElementById("search-anime-btn");
+        let url ='https://api.jikan.moe/v3/search/anime?q=naruto'
+
+        let dataCardTemplate = document.querySelector("[data-template]")
+        let dataContainer = document.querySelector("[data-container]")
+        
+        let searchInput = document.querySelector("[data-search]");
         let autocomplete = document.getElementById("autocomplete");
+        let animeData = [];
+
+        fetchData()
+    
+
         
-        function handleAnime(dat) {
+        searchInput.addEventListener("input", (e) => {
+            const value = e.target.value;
+            
+            animeData.forEach(anime => {
+            const isVisible = anime.name.includes(value) || anime.image.includes(value)
+            anime.el.classList.toggle("hide", !isVisible)
+            })
+        })
         
+        function displayInfo(data) {
+
+            
+            animeData = data.map(element => {
+            const card = dataCardTemplate.content.cloneNode(true).children[0];
+            const header = card.querySelector("[data-name]")
+            const bodyImg = card.querySelector("[data-img]")
+            const button = card.querySelector("[data-btn]")    
+            header.textContent = element.title;
+            bodyImg.src = element.image_url;
+            button.textContent = " Like ❤️ ";
+            dataContainer.append(card);
+            
+            return { name: element.title, image: element.image_url, el: card }
+            /*
             let div = document.createElement("div");
             let h2 = document.createElement("h2");
-            let image = document.createElement("img");
+            let ima = document.createElement("img");
             //let p = document.createElement("p");
             let button = document.createElement("button");
         
             div.className = "card";
-            h2.textContent = dat.title;
-            image.src = dat.image_url;
-            image.className = "anime-avatar";
+            h2.textContent = element.title;
+            ima.src = element.image_url;
+            ima.className = "anime-avatar";
             // p.textContent = dat.synopsis;
             button.className = "like-btn";
-            button.setAttribute("id", dat.id);
+            button.setAttribute("id", element.id);
             button.textContent = " Like ❤️ "
-            div.append(h2, image, button);
-            divCard.append(div);
+            div.append(h2, ima, button);
+
+            dataContainer.append(div);
         
-        
-            button.addEventListener("click", function(e) {
-                handleLikes(e, elemen, p);
-        
-            })
-        }
-        
-                
-        function displayInfo(data) {
-        
-            data.forEach(element => {
-                handleAnime(element)
+            */
         
             })
         }
         
         
-        let url ='https://api.jikan.moe/v3/search/anime?q=naruto'
         
         function fetchData (){
         fetch('https://api.jikan.moe/v3/search/anime?q=naruto')
         .then(res=>res.json())
         .then(data=>displayInfo(data.results))
         
+
         }
-        fetchData()
+
         
+    /*
         function renderForm(){
             let formContainer = document.querySelector('body')
             let form = document.createElement('form')
@@ -77,7 +99,6 @@
             dateInput.name = 'date'
         
         
-        
             form.append(dateInput,nameInput,companyInput,submitBtn)
             formContainer.append(form)
             form.addEventListener('submit',(e)=>{
@@ -96,3 +117,4 @@
             })
         }
         renderForm()
+        */
